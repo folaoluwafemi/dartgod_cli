e14d7727e3bec735ac86c5c7def5376fd2ddbc25
folaoluwfemi <folaoluwafemi55@gmail.com>
2023-02-04 19:37:11 +0100

    initial commit

---
+++ bin/dartgod_cli.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:dartgod_cli/dartgod_cli.dart' as dartgod_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartgod_cli.calculate()}!');
+
+}
---
+++ test/dartgod_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartgod_cli/dartgod_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
